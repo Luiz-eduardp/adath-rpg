46cd9eece051359219c9917db5eab14339fc5fa6
Luiz Eduardo <luizzeduardp@gmail.com>
2021-12-01 16:51:47 -0300

    v1

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,45 @@
 <template>
-  <div id="app">
-    <div id="nav">
+  
+<v-app>
+  <v-navigation-drawer app>
+     <ul>
+       <li>
+         menu
+       </li>
+     </ul>
+    
+  </v-navigation-drawer>
+
+  <v-app-bar app>
+ 
+<div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
-  </div>
+  </v-app-bar>
+
+  <!-- Sizes your content based upon application components -->
+  <v-main>
+
+    <!-- Provides the application the proper gutter -->
+    <v-container fluid>
+
+      <!-- If using vue-router -->
+      <router-view></router-view>
+    </v-container>
+  </v-main>
+
+  <v-footer app>
+luiz  </v-footer>
+</v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
+
 }
+</script>
 
-#nav {
-  padding: 30px;
+<style>
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
---
+++ src/components/fixed/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  
+<div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+#nav{
+
+}
+</style>
